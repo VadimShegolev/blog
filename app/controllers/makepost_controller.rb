809c697371e16be814a289c5abd8392c3602c6fd
VadimShegolev <keepman12121998@gmail.com>
2021-11-17 23:00:35 +0600

    Auth

---
+++ app/controllers/makepost_controller.rb
@@ -0,0 +1,14 @@
+class MakepostController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        # do something
+      end
+    
+      def show
+        # do something
+      end
+    
+      def create
+        # do something
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   devise_for :users, controllers: {
+#     sessions: 'users/sessions',
+#     registrations: 'users/registrations',
+#   }
+#   devise_scope :user do
+#     get 'sign_in', to: 'users/sessions#new'
+#     get 'sign_up', to: 'users/registrations#new'
+#     get 'forgot_password', to: 'users/passwords#new'
+#     get 'reset_password', to: 'users/passwords#edit'
+
+
+# end
+
+
+  # devise_for :users
+  root to: "home#index"
+  get 'home/index', as: 'user_root'
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  
 end
