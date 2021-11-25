6b699c9892bb127165762bb310d7c2e8daaa3210
VadimShegolev <keepman12121998@gmail.com>
2021-11-26 01:29:18 +0600

    Phone param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:email, :phone, :password, :password_confirmation, :remember_me]
+    added_attrs = [:phone, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
