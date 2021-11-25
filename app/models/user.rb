75f621e5b6b58356dfb4552924e7a7cc1eda97f1
VadimShegolev <keepman12121998@gmail.com>
2021-11-26 01:00:58 +0600

    Phone param

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,42 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [ :login ]
+
+  attr_writer :login
+
+  def login
+    @login || self.phone || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(phone) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:phone].nil?
+        where(conditions).first
+      else
+        where(phone: conditions[:phone]).first
+      end
+    end
+  end
+
+  validates_format_of :phone, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  # validate :validate_phone
+
+  # def validate_phone
+  #   if User.where(phone: phone).exists?
+  #     errors.add(:phone, :invalid)
+  #   end
+  # end
+
+  # validate :validate_email
+
+  # def validate_email
+  #   if User.where(email: email).exists?
+  #     errors.add(:email, :invalid)
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_095008) do
+ActiveRecord::Schema.define(version: 2021_11_25_173003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_095008) do
     t.string "last_name"
     t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
